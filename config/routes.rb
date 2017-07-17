7da7be839c70628e3eba74d3e553da873e947e8b
mraballard <mraballard@gmail.com>
2017-07-17 11:59:22 -0400

    Add users controller with signup route, with tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   root 'static_pages#home'
 
   get '/help', to: 'static_pages#help', as: 'help'
@@ -6,6 +7,8 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about'
 
   get 'contact', to: 'static_pages#contact'
+
+  get '/signup', to: 'users#new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
